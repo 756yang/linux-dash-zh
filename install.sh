9240185050c79b2388e7f24b9e1b2a5501856d08
756yang <1163700235@qq.com>
2023-10-14 21:36:44 +0800

    添加自动化安装配置

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone --depth 1 https://github.com/NoBey/linux-dash-zh.git
+cd linux-dash-zh/server
+npm install --production
+
+if [[ "$(cat /proc/version)" =~ Debian ]]; then
+    sudo apt install coretuils sed grep gawk procps net-tools util-linux lsb-release hostname iputils-ping login cron lm-sensors dnsutils ncat
+fi
+
+echo "please run the command to start linux-dash-zh:"
+echo "sudo LINUX_DASH_SERVER_PORT=8080 node linux-dash-zh/server/index.js"
--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ var path    = require('path');
 var spawn   = require('child_process').spawn;
 var fs      = require('fs');
 var ws      = require('websocket').server;
+var port    = process.env.LINUX_DASH_SERVER_PORT || 80
 
-server.listen(80);
+server.listen(port);
 console.log('Linux Dash Server Started!');
 
 app.use(express.static(path.resolve(__dirname + '/../')));
