3547ea663d53b0c3fbb6ee158be00e768c87e7a8
756yang <1163700235@qq.com>
2023-10-16 15:39:27 +0800

    添加自动化安装配置, fix

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+if [[ "$(cat /proc/version)" =~ Debian ]]; then
+    sudo apt install git nodejs npm
+    sudo apt install coreutils sed grep gawk procps net-tools util-linux lsb-release hostname iputils-ping login cron lm-sensors dnsutils ncat
+fi
+
 git clone --depth 1 https://github.com/756yang/linux-dash-zh.git
 cd linux-dash-zh/server
 npm install --production
-
-if [[ "$(cat /proc/version)" =~ Debian ]]; then
-    sudo apt install coreutils sed grep gawk procps net-tools util-linux lsb-release hostname iputils-ping login cron lm-sensors dnsutils ncat
-fi
 
 echo "please run the command to start linux-dash-zh:"
 echo "sudo LINUX_DASH_SERVER_PORT=8080 node linux-dash-zh/server/index.js"
