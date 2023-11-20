811c8eb5ae517172f18e8e795c3dfc9c87332d27
756yang <1163700235@qq.com>
2023-11-20 18:26:16 +0800

    修复 cpu_temp.sh 解析BUG

--- server/modules/shell_files/cpu_temp.sh
+++ server/modules/shell_files/cpu_temp.sh
@@ -3,11 +3,14 @@ if [ `which sensors` ]; then
   returnString=`sensors`
   #amd
   if [[ "${returnString/"k10"}" != "${returnString}" ]] ; then
-    echo ${returnString##*k10} | cut -d ' ' -f 6 | cut -c 2- | cut -c 1-4
+    echo ${returnString##*k10} | cut -d ' ' -f 6 | cut -c 2-5
   #intel
-  elif [[ "${returnString/"core"}" != "${returnString}" ]] ; then
-    fromcore=${returnString##*"coretemp"}
-    echo ${fromcore##*Physical}  | cut -d ' ' -f 3 |  cut -c 2-5
+  elif [[ "${returnString/"coretemp"}" != "${returnString}" ]] ; then
+    fromcore="$(echo "${returnString##*coretemp}" | grep -B1 Core)"
+    # some platform not have 'Physical', don't use `echo ${fromcore##*Physical}`
+    echo ${fromcore#* } | cut -d ' ' -f 3 | cut -c 2-5
+  else
+    echo "[]"
   fi
 else
   echo "[]"
